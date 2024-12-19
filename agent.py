aad60c20de481004fb8cebef3fd5c1703442d29e
Zhu Chen <zhuchencs@126.com>
2024-12-19 16:59:35 +0800

    agent doing

--- agent.py
+++ agent.py
@@ -9,6 +9,16 @@ def main():
     agent = DQN(5e-5, env.observation_space.shape[0], env.action_space.n, 256, 256, None, 0.005, 0.99, 1, 0.05, 5e-4, 10000, 256)
 
     for i in range(max_episodes):
-        init_obs = env.reset()[0]
-        action = agent.choose_action(init_obs, isTrain=True)
-        next_state, reward, terminal, truncated, _ = env.step(action)
+        total_reward_per_epi = 0
+        state = env.reset()[0]
+        while (True): 
+            action = agent.choose_action(state, isTrain=True)
+            next_state, reward, terminal, truncated, _ = env.step(action)
+            agent.replay_buffer.store_once(state, action, reward, next_state, terminal)
+            if terminal or truncated:
+                continue
+            state = next_state
+        
+
+
+        
